68edc47a731222122e543f9163afecd95aa2a49d
Inna <prizrak1609@gmail.com>
2023-10-12 17:47:41 +0300

    Додала оператор+, оператор- та оператори порівняння

--- Magazine.cs
+++ Magazine.cs
@@ -3,11 +3,21 @@
 //Реалізуйте методи класу для введення даних, виведення даних.
 //Реалізуйте доступ до окремих полів через методи класу.
 
+//Додайте до вже створеного класу «Журнал» інформацію про кількість працівників.
+//Виконайте навантаження + (для збільшення кількості працівників на вказану кількість),
+// — (для зменшення кількості працівників на вказану кількість),
+// == (перевірка на рівність кількості працівників),
+// < і > (перевірка на меншу чи більшу кількість працівників),
+// != і Equals.Використовуйте механізм властивостей полів класу.
+
+using NET_Homework_3_2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NET_Homework_3_2
 {
@@ -18,21 +28,34 @@ namespace NET_Homework_3_2
         public string Description { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public Magazine(string name, string year, string description, string phone, string email)
+        public int NumberEmployees { get; set; }
+
+        public Magazine()
         {
-            Name = name;
-            Year = year;
-            Description = description;
-            Phone = phone;
-            Email = email;
+            Name = null;
+            Year = null;
+            Description = null;
+            Phone = null;
+            Email = null;
+            NumberEmployees = 0;
         }
-        public void InputData(string name, string year, string description, string phone, string email)
+        public Magazine(string name, string year, string description, string phone, string email, int number)
         {
             Name = name;
             Year = year;
             Description = description;
             Phone = phone;
             Email = email;
+            NumberEmployees = number;
+        }
+        public void InputData(string name, string year, string description, string phone, string email, int number)
+        {
+            Name = name;
+            Year = year;
+            Description = description;
+            Phone = phone;
+            Email = email;
+            NumberEmployees = number;
         }
         public void OtputData()
         {
@@ -40,7 +63,80 @@ namespace NET_Homework_3_2
             Console.WriteLine($"Рік заснування: {Year}");
             Console.WriteLine($"Опис журналу: {Description}");
             Console.WriteLine($"Контактний телефон: {Phone}");
-            Console.WriteLine($"email: {Email}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Кількість працівників: {NumberEmployees}");
         }
+
+        public void Comparison(Magazine m1, Magazine m2)
+        {
+            if (m1.NumberEmployees != m2.NumberEmployees)
+            {
+                Console.WriteLine("\nКількість працівників в журналах не рівна:");
+                if (m1.NumberEmployees < m2.NumberEmployees)
+                {
+                    Console.WriteLine($"Кількість працівників в журналі \"{m2.Name}\"({m2.NumberEmployees}) більша ніж в журналі \"{m1.Name}\"({m1.NumberEmployees})");
+                }
+                else if (m1.NumberEmployees > m2.NumberEmployees)
+                {
+                    Console.WriteLine($"Кількість працівників в журналі \"{m1.Name}\"({m1.NumberEmployees}) більша ніж в журналі \"{m2.Name}\"({m2.NumberEmployees})");
+                }
+            }
+            else if (m1.NumberEmployees == m2.NumberEmployees)
+            {
+                Console.WriteLine("\nКількість працівників в журналах рівна:");
+                Console.WriteLine($"Кількість працівників в журналі \"{m1.Name}\"({m1.NumberEmployees}) рівна кількості працівників в журналі \"{m2.Name}\"({m2.NumberEmployees})");
+            }
+        }
+        public static Magazine operator +(Magazine magazine, int n)
+        {
+            magazine.NumberEmployees += n;
+            return magazine;
+        }
+        public static Magazine operator -(Magazine magazine, int n)
+        {
+            magazine.NumberEmployees -= n;
+            return magazine;
+        }
+        public static bool operator ==(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberEmployees == magazine2.NumberEmployees;
+        }
+        public static bool operator !=(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberEmployees != magazine2.NumberEmployees;
+        }
+        public static bool operator <(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberEmployees < magazine2.NumberEmployees;
+        }
+        public static bool operator >(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberEmployees > magazine2.NumberEmployees;
+
+        }
+        public static bool Equals(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1 != magazine2;
+        }
+
+
+        /*public override bool Equals (object obj)
+        {
+            if (obj is Magazine m)
+            {
+                return this.Name != m.Name &&
+                       this.Year != m.Year &&
+                       this.Description != m.Description &&
+                       this.Phone != m.Phone &&
+                       this.Email != m.Email &&
+                       this.NumberEmployees != m.NumberEmployees;
+            }
+            return true;
+        }*/
+
+       /* public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Year, Description, Phone, Email, NumberEmployees);
+        }*/
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@
 //Реалізуйте методи класу для введення даних, виведення даних.
 //Реалізуйте доступ до окремих полів через методи класу.
 
+//Додайте до вже створеного класу «Журнал» інформацію про кількість працівників.
+//Виконайте навантаження + (для збільшення кількості працівників на вказану кількість),
+// — (для зменшення кількості працівників на вказану кількість),
+// == (перевірка на рівність кількості працівників),
+// < і > (перевірка на меншу чи більшу кількість працівників),
+// != і Equals.Використовуйте механізм властивостей полів класу.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +23,40 @@ namespace NET_Homework_3_2
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Magazine magazine = new Magazine("Мій журнал", "2020", "Опис мого журнала", "2233445566", "my_address@gmail.com");
+            Magazine magazine1 = new Magazine("Мій журнал", "2020", "Опис мого журнала", "2233445566", "my_address@gmail.com", 10);
             Console.WriteLine("Дані мого журнала:");
-            magazine.OtputData();
-
-            Console.WriteLine();    
-            magazine.InputData("Новий журнал", "2023", "Опис нового журнала", "9988776655", "new_address@gmail.com");
+            magazine1.OtputData();
+                      
+            Console.WriteLine(); 
+            Magazine magazine2 = new Magazine();
+            magazine2.InputData("Новий журнал", "2023", "Опис нового журнала", "9988776655", "new_address@gmail.com", 12);
             Console.WriteLine("Нові дані журнала:");
-            magazine.OtputData();
+            magazine2.OtputData();
+
+            magazine1.Comparison(magazine1, magazine2);
+
+            magazine1.NumberEmployees = magazine1.NumberEmployees + 5;
+            Console.WriteLine($"\nКількість працівників в журналі \"{magazine1.Name}\" збільшено на 5 чоловік : {magazine1.NumberEmployees}");
+
+            magazine1.Comparison(magazine1, magazine2);
+
+            magazine2.NumberEmployees = magazine2.NumberEmployees - 8;
+            Console.WriteLine($"\nКількість працівників в журналі \"{magazine2.Name}\" зменшено на 8 чоловік : {magazine2.NumberEmployees}");
+
+            magazine1.Comparison(magazine1, magazine2);
+
+            magazine2.NumberEmployees = magazine2.NumberEmployees + 11;
+            Console.WriteLine($"\nКількість працівників в журналі \"{magazine2.Name}\" збільшено на 11 чоловік : {magazine2.NumberEmployees}");
+
+            magazine1.Comparison(magazine1, magazine2);
+
+            magazine2.Equals(magazine1);
+
+            magazine1 = magazine2;
+            Magazine magazine = new Magazine();
+            Console.WriteLine($"Порівняння журналів Equals (magazine1, magazine2): {Equals(magazine1, magazine2)}");
+            Console.WriteLine($"Порівняння журналів Equals (magazine1, magazine): {Equals(magazine1, magazine)}");
+
             Console.ReadLine();
         }
     }
